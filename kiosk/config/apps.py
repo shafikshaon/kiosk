c071bf8924ed3a974a3b0f162c359d052804b178
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-13 14:24:29 +0600

    Custom and and django apps separate

---
+++ kiosk/config/apps.py
@@ -0,0 +1,18 @@
+# Applications
+
+DJANGO_DEFAULT_APPS = [
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+CUSTOM_APPS = [
+    'gist'
+]
+
+SITE_ID = 1
+
+INSTALLED_APPS = DJANGO_DEFAULT_APPS + CUSTOM_APPS
