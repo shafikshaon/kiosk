697de5ef34cf1e36d278eb761ed7ee9030f06a7a
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-13 18:50:59 +0600

    dashboard view sample view add and url configured

---
+++ gist/urls.py
@@ -0,0 +1,4 @@
+__author__ = 'Shafikur Rahman'
+
+urlpatterns = [
+]
---
+++ gist/views/dashboard.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+
+__author__ = 'Shafikur Rahman'
+
+
+class DashboardView(TemplateView):
+    pass
