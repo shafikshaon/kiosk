3a7dd086fda6ae783d48772c9fc945c4c698b479
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-13 12:06:48 +0600

    activity, base, key, timelog abstract model add

---
+++ gist/models/key.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from gist.models.base import Base
+from gist.utils.generate_random_number import generate_random_number
+
+__author__ = 'Shafikur Rahman'
+
+
+class Key(Base):
+    id = models.BigAutoField(primary_key=True)
+    main_key = models.BigIntegerField(unique=True, default=generate_random_number, editable=False)
+    code = models.CharField(max_length=20, null=False, blank=False)
+
+    class Meta:
+        abstract = True
