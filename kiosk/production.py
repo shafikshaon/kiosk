a47e32c66a806b5db4f6619f617720617c00343f
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-13 14:29:14 +0600

    production configuration add

---
+++ kiosk/production.py
@@ -0,0 +1,22 @@
+from .config import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+]
+
+INSTALLED_APPS += THIRD_PARTY_APPS
+
+INTERNAL_IPS = ['127.0.0.1', 'localhost']
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
+    }
+}
