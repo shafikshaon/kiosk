a610b61a345aa63e4572d8af6de1f609a31f91ef
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-14 23:00:24 +0600

    * role add url register * form field focus css change

--- authorization/urls/urls.py
+++ authorization/urls/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
+
+from authorization.views.role import RoleAddView
 
 __author__ = "Shafikur Rahman"
 
 urlpatterns = [
+    path('role/add/', RoleAddView.as_view(), name='role-add'),
 ]
