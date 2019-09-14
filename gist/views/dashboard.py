3223feecbdfb3f90c98d1d63f93ee250e2a9903c
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-14 17:07:41 +0600

    separate dashboard, breadcrumb, footer, side navigation, and top navigation bar

--- gist/views/dashboard.py
+++ gist/views/dashboard.py
@@ -5,3 +5,9 @@ __author__ = 'Shafikur Rahman'
 
 class DashboardView(TemplateView):
     template_name = 'admin/gist/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['title'] = 'Dashboard'
+        context['page_headline'] = 'Dashboard'
+        return context
