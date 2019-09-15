6ea88b0a189f7c53c022286d21d8990d6e49c02b
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-16 00:12:37 +0600

    searchable columns and order by columns dict convert to sentence case

--- gist/templatetags/sentence_case.py
+++ gist/templatetags/sentence_case.py
@@ -5,4 +5,25 @@ register = template.Library()
 
 @register.filter(name='sentence_case')
 def sentence_case(value):
-    return value.capitalize()
+    item_list = value.split(':')
+    item_length = len(item_list)
+    column = {}
+    if item_length == 1:
+        value = item_list[0].replace('_', ' ')
+        column.update({'name': value})
+        value = value.capitalize()
+        column.update({'display_name': value})
+        column.update({'type': 'text'})
+        return column
+    if item_length == 2:
+        column.update({'name': item_list[0]})
+        value = item_list[1].replace('_', ' ')
+        column.update({'display_name': value})
+        column.update({'type': 'text'})
+        return column
+    if item_length == 3:
+        column.update({'name': item_list[0]})
+        value = item_list[1].replace('_', ' ')
+        column.update({'display_name': value})
+        column.update({'type': item_list[2]})
+        return column
