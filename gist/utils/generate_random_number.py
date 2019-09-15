ed304027ffc2cfd7f6d4d5f1eca0b3a540cc164b
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-16 00:55:20 +0600

    update generate_random_number util method for pk

--- gist/utils/generate_random_number.py
+++ gist/utils/generate_random_number.py
@@ -1,22 +1,8 @@
 from datetime import datetime
-from random import random
 
 __author__ = 'Shafikur Rahman'
 
 
 def generate_random_number():
-    now = datetime.now()
-    generated_number = ''
-    generated_number += now.microsecond
-    generated_number += now.second
-    generated_number += now.minute
-    generated_number += now.hour
-    generated_number += now.day
-    generated_number += now.month
-    generated_number += now.year
-
-    try:
-        generated_number = int(generated_number)
-    except ValueError:
-        generated_number = random.range(10000000, 99999999)
+    generated_number = int(datetime.now().timestamp() * 1000)
     return generated_number
