f21be234af1b650f0e0998686664f65c4094c142
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-16 21:44:36 +0600

    role detail template add and detail view url registered

--- authorization/urls/urls.py
+++ authorization/urls/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from authorization.views.role import RoleAddView, RoleListView
+from authorization.views.role import RoleAddView, RoleListView, RoleDetailView
 
 __author__ = "Shafikur Rahman"
 
 urlpatterns = [
     path('role/add/', RoleAddView.as_view(), name='role-add'),
     path('role/list/', RoleListView.as_view(), name='role-list'),
+    path('role/view/<pk>/', RoleDetailView.as_view(), name='role-view'),
 ]
