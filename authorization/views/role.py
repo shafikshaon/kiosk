02569632d8f9c310bf7e3e5091dd87add2e2caf9
Mohd. Shafikur Rahman Shaon <shafikshaon@gmail.com>
2019-09-16 21:39:50 +0600

    RoleDetailView view implemented

--- authorization/views/role.py
+++ authorization/views/role.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from authorization.forms.role import RoleForm
 from authorization.models import Role
@@ -14,7 +15,7 @@ __author__ = 'Shafikur Rahman'
 class RoleAddView(LoginRequiredMixin, CreateView):
     template_name = 'admin/authorization/role/add.html'
     form_class = RoleForm
-    login_url = 'admin/accounts/login/'
+    login_url = settings.ADMIN_LOGIN_REDIRECT_URL
 
     def get_context_data(self, **kwargs):
         context = super(RoleAddView, self).get_context_data(**kwargs)
@@ -37,7 +38,7 @@ class RoleAddView(LoginRequiredMixin, CreateView):
         return self.render_to_response(self.get_context_data(form=form))
 
 
-class RoleListView(ListViewMixin):
+class RoleListView(LoginRequiredMixin, ListViewMixin):
     template_name = 'admin/authorization/role/list.html'
     model = Role
     paginate_by = 10
@@ -55,4 +56,21 @@ class RoleListView(ListViewMixin):
         search = self.request.GET.get('search1', None)
         if search:
             object_list = object_list.filter(name__icontains=search)
-        return object_list.filter(is_delete=False).values('pk', 'name', 'add_by', 'is_delete', 'add_at')
+        return object_list.filter(is_delete=False).values('pk', 'name', 'add_by__username', 'is_delete', 'add_at')
+
+
+class RoleDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'admin/authorization/role/detail.html'
+    model = Role
+    login_url = settings.ADMIN_LOGIN_REDIRECT_URL
+
+    def get_context_data(self, **kwargs):
+        context = super(RoleDetailView, self).get_context_data(**kwargs)
+        context['title'] = 'Role - Details'
+        context['page_headline'] = context['object'].name
+        context['permissions'] = context['object'].permissions.values('name')
+        return context
+
+    def get_queryset(self):
+        queryset = super(RoleDetailView, self).get_queryset()
+        return queryset.select_related('add_by', 'change_by')
